25518a051ba6840487a8a143afa7f6d6f6e78ea6
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-02-22 13:15:18 +0000

    finish popup

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const fifth = document.querySelector(".fifth");
-const profile = document.querySelector(".fifth .profile");
-const popup = document.querySelector(".popup");
+const sixth = document.querySelector(".sixth");
+//const profile = document.querySelector(".fifth .profile");
+const profile = document.querySelector(".profile");
+const popup = document.querySelector(".fifth .popup");
+const sixthPopup = document.querySelector(".sixth .popup");
+const heading = document.querySelector(".sixth .heading");
 
 profile.addEventListener("click", (event) => {
   popup.classList.add("visible");
   event.stopPropagation();
+});
+
+heading.addEventListener("click", (event) => {
+  sixthPopup.classList.add("visible");
+  event.stopPropagation();
+});
+
+sixth.addEventListener("click", () => {
+  sixthPopup.classList.remove("visible");
 });
 
 fifth.addEventListener("click", () => {
