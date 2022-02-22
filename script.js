dbf159028903ee69f0ed4bbb805d4f1c95d2c49f
Eeman Sayed <eeman.sayed.12@gmail.com>
2022-02-22 13:33:54 +0000

    styel

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const fifth = document.querySelector(".fifth");
+const profile = document.querySelector(".fifth .profile");
+const sixthProfile = document.querySelector(".sixth .right .profile");
 const sixth = document.querySelector(".sixth");
-//const profile = document.querySelector(".fifth .profile");
-const profile = document.querySelector(".profile");
+//const profile = document.querySelector(".profile");
 const popup = document.querySelector(".fifth .popup");
 const sixthPopup = document.querySelector(".sixth .popup");
+const sixthRightPopup = document.querySelector(".sixth .right .popup");
+const rightPopup = document.querySelector(".sixth .right .popup");
 const heading = document.querySelector(".sixth .heading");
 
 profile.addEventListener("click", (event) => {
   popup.classList.add("visible");
   event.stopPropagation();
+});
+
+fifth.addEventListener("click", () => {
+  popup.classList.remove("visible");
 });
 
 heading.addEventListener("click", (event) => {
@@ -18,8 +25,11 @@ heading.addEventListener("click", (event) => {
 
 sixth.addEventListener("click", () => {
   sixthPopup.classList.remove("visible");
+  sixthRightPopup.classList.remove("visible");
 });
 
-fifth.addEventListener("click", () => {
-  popup.classList.remove("visible");
+sixthProfile.addEventListener("click", (event) => {
+  rightPopup.classList.add("visible");
+  console.log(sixthProfile);
+  event.stopPropagation();
 });
